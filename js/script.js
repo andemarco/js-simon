4bc25c2c47a56eda64289e437fd3144b1ef1413a
Andrea <andreademarco91@gmail.com>
2020-01-21 16:48:13 +0100

    function memory OK

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-// Un alert espone 5 numeri casuali.
-// Da li parte un timer di 30 secondi.
-// Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
 
-// ARRAY in cui inserire i numeri random generati
-var randomNumbers = [];
+var randomNumbers = []; //array numeri random generati
+var userNumbers = []; //array numeri inseriti dall'utente
+
 // GENERO ED INSERISCO NUMERI IN ARRAY
 for (var i = 0; i < 5; i++) {
   randomNumbers.push(getRandomNumber(1, 100))
@@ -13,15 +10,33 @@ console.log(randomNumbers);
 
 // IMPOSTO TIMEOUT DI 30 secondi
 
+setTimeout(askNumber, 1000);
+
+// SE RANDOMNUMBERS INCLUDE UN ELEMENTO DI USERNUMBERS INSERISCO UN +1 TROVATO e LO PUSHO AD ARRAY DEI TROVATI
+
+var foundNumbers = [] //arry numeri TROVATI
+var found = 0; //numero di numeri trovati
 
 
 // CREO FUNZIONE PROMPT INSERIMENTO numeri
-var userNumbers = [];
-for (var i = 0; i < 5; i++) {
-  userNumbers.push(parseInt(prompt('inserisci i numeri')))
+function askNumber() {
+  while (userNumbers.length < 5) {
+    userNumbers.push(parseInt(prompt('inserisci i numeri')))
+  }
+  var found = 0; //numero di numeri trovati
+  for (var i = 0; i < randomNumbers.length; i++) {
+    if (randomNumbers.includes(userNumbers[i])) {
+      found++;
+      foundNumbers.push(userNumbers[i])
+    }
+  }
 }
-console.log(userNumbers);
+console.log(randomNumbers, 'i numeri random');
+console.log(userNumbers, 'numeri utente');
+console.log(foundNumbers, 'numeri trovati');
+console.log(found, 'quanti numeri hai trovato');
 // FUNZIONE PER NUMERI RANDOM
+
 function getRandomNumber (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
