67b7851d1df29974796437c4dd52800b72429ad5
Andrea <andreademarco91@gmail.com>
2020-01-21 15:16:08 +0100

    Array random numbers

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Un alert espone 5 numeri casuali.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
+
+// ARRAY in cui inserire i numeri random generati
+var randomNumbers = [];
+// INSERISCO NUMERI IN ARRAY
+for (var i = 0; i < 5; i++) {
+  randomNumbers.push(getRandomNumber(1, 100))
+}
+console.log(randomNumbers);
+
+
+
+// FUNZIONE PER NUMERI RANDOM
+function getRandomNumber (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
